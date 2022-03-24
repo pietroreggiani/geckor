fcd97fd7d92bdc37968cceab4a8813a064839950
Pietro Reggiani <preggian@stern.nyu.edu>
2022-03-24 10:22:39 -0400

    edited package initial message

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,3 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("R client for the CoinGecko API\nDeveloped by Next Game Solutions (http://nextgamesolutions.com)")
+  packageStartupMessage("R client for the CoinGecko API. \n Developed by Next Game Solutions (http://nextgamesolutions.com) \n Edited by Pietro Reggiani to fit his research needs. for ")
 }
